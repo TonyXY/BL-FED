08c20e95934c8a3aa5b446c47c606a25a3e32ac7
肖阳 <yx8826@163.com>
2017-07-23 23:45:22 +0800

    更新关于页面内容

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,29 +1,16 @@
 <template>
 	<div class="bl-about">
-		<Timeline>
-			<Timeline-item>
-				<p class="time">1976年</p>
-				<p class="content">Apple I 问世</p>
-			</Timeline-item>
-			<Timeline-item>
-				<p class="time">1984年</p>
-				<p class="content">发布 Macintosh</p>
-			</Timeline-item>
-			<Timeline-item>
-				<p class="time">2007年</p>
-				<p class="content">发布 iPhone</p>
-			</Timeline-item>
-			<Timeline-item>
-				<p class="time">2010年</p>
-				<p class="content">发布 iPad</p>
-			</Timeline-item>
-			<Timeline-item>
-				<p class="time">2011年10月5日</p>
-				<p class="content">史蒂夫·乔布斯去世</p>
-			</Timeline-item>
-		</Timeline>
+		<h3>关于我</h3>
+		<p>现居上海浦东，大学学的是数控专业；2011年转行从事IT工作，2011-2014年做php开发，2014年开始做前端开发；热爱前端工作，喜欢研究技术。</p>
+		<p>网名：小鱼的理想</p>
+		<p>邮箱：yx8826#163.com （#改为@）</p>
+		<p>GitHub：https://github.com/TonyXY</p>
 	</div>
 </template>
+<style>
+
+</style>
+
 <script>
 
 export default {
