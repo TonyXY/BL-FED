29e4559445a7c97b44dd2eb6d8ccbb662fca0de3
肖阳 <yx8826@163.com>
2017-10-23 18:55:07 +0800

    update vscodeUse.md

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
-
-<div id="app" class="layout">
-    <router-view name="header"></router-view>
-    <router-view></router-view>
-    <router-view name="footer"></router-view>
-</div>
+    <div id="app" class="layout">
+        <router-view name="header"></router-view>
+        <router-view></router-view>
+        <router-view name="footer"></router-view>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'app',
-    data: function () {
-        return {
-        };
+    name: "app",
+    data: function() {
+        return {};
     }
-}
+};
 </script>
 
 <style lang="scss">
-@import './assets/css/scss/style.scss';
+@import "./assets/css/scss/style.scss";
 </style>
